afb2d8ef30ae8cf6679d92aa9772b48e4364650d
NoHop3 <u_sux@abv.bg>
2021-11-02 12:04:04 +0100

    Connection with 2nd tier (not final) using JSON

--- ApplicationTier/Data/Impl/UserData.cs
+++ ApplicationTier/Data/Impl/UserData.cs
@@ -1,4 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using ApplicationTier.Models;
 
@@ -6,17 +12,77 @@ namespace ApplicationTier.Data.Impl
 {
     public class UserData : IUserService
     {
-        
-        
+        private string ip;
+        private int port;
+        private IList<User> _users;
+        private TcpClient _client;
+        private NetworkStream _stream;
+        public UserData(string ip, int port)
+        {
+            this.ip = ip;
+            StartConnection();
+        }
+
+        private void StartConnection()
+        {
+            IPAddress ipAddress = IPAddress.Parse(ip);
+            TcpListener listener = new TcpListener(ipAddress, port);
+            listener.Start();
+            while (true)
+            {
+                try
+                {
+                    _client = listener.AcceptTcpClient();
+                    Console.WriteLine("Connection initiated");
+                    _stream = _client.GetStream();
+                    FetchData();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    CloseConnection();
+                }
+            }
+        }
+
+        private async Task FetchData()
+        {
+            try
+            {
+                StreamReader streamReader = new StreamReader(_stream, Encoding.UTF8);
+                string dataReceived = streamReader.ReadLine();
+
+                if (!string.IsNullOrEmpty(dataReceived))
+                {
+                    _users = JsonSerializer.Deserialize<List<User>>(dataReceived);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                CloseConnection();
+            }
+        }
+
+        private void CloseConnection()
+        {
+            _client.Close();
+        }
         
         public Task<User> ValidateUserAsync(string username, string password)
         {
-            throw new System.NotImplementedException();
+            StreamWriter streamWriter = new StreamWriter(_stream, Encoding.UTF8);
+            User user = new User();
+            user.Username = username;
+            user.Password = password;
+            string userToJson = JsonSerializer.Serialize(user);
+            streamWriter.WriteLineAsync(userToJson);
+            throw new NotImplementedException();
         }
 
-        public Task<IList<User>> GetUsersAsync()
+        public async Task<IList<User>> GetUsersAsync()
         {
-            throw new System.NotImplementedException();
+            return _users;
         }
     }
 }
