b44f55240c3bbb35441d1aa648fc94cfa69b2427
NoHop3 <u_sux@abv.bg>
2021-11-02 10:50:54 +0100

    First steps of 1st to 2nd tier connection + user page commit

---
+++ ApplicationTier/Data/Impl/UserData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ApplicationTier.Models;
+
+namespace ApplicationTier.Data.Impl
+{
+    public class UserData : IUserService
+    {
+        
+        
+        
+        public Task<User> ValidateUserAsync(string username, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IList<User>> GetUsersAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
