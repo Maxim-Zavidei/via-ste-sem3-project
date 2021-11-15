e9e8b64ded4249a2b5e3feb638cd2b7d08f9a9b9
Maxim-Zavidei <maxim-zavidei+github@pm.me>
2021-11-15 19:47:05 +0100

    Implemented a nav bar and a drawer

--- ApplicationTier/Startup.cs
+++ ApplicationTier/Startup.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -13,21 +7,21 @@ using ApplicationTier.Data;
 using ApplicationTier.Data.Impl;
 using Microsoft.AspNetCore.Components.Authorization;
 using Authentication;
+using MudBlazor.Services;
 
 namespace ApplicationTier {
     public class Startup {
+        
         public Startup(IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services) {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+            services.AddMudServices();
             services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
             //services.AddScoped<ICommunicator, Communicator>();
             services.AddScoped<IUserService, UserData>();
