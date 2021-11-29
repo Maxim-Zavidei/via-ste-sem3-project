a8cba58cc69717837d2866a112baf0a19c9821bd
Valeriu1 <roscavaleriuvaleriu@gmail.com>
2021-11-29 14:28:31 +0100

    Added the modified classes(Address, Event) to ApplicationTier from DataAccessTier

--- ApplicationTier/Models/Address.cs
+++ ApplicationTier/Models/Address.cs
@@ -5,26 +5,22 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace ApplicationTier.Models {
-    public class Event {
+    public class Address {
         [JsonPropertyName("id")]
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id {get;set;}
+        [JsonPropertyName("streetName")]
+        public string StreetName { get; set; }
         
-        [JsonPropertyName("title")]
-        public string Title { get; set; }
+        [JsonPropertyName("number")]
+        public string Number { get; set; }
         
-        [JsonPropertyName("description")]
-        public string Description { get; set; }
+        [JsonPropertyName("city")]
+        public string City { get; set; }
         
-        [JsonPropertyName("startTime")]
-        public DateTime StartTime { get; set; }
-        
-        [JsonPropertyName("endTime")]
-        public DateTime EndTime { get; set; }
-
-        [JsonPropertyName("address")]
-        public Address Address { get; set; }
+        [JsonPropertyName("country")]
+        public string Country { get; set; }
 
         public override string ToString() {
             return JsonSerializer.Serialize(this);
