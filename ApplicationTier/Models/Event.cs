ac2e37e661bffda7ce8adfc18722779f8ee9db73
Maxim-Zavidei <maxim-zavidei+github@pm.me>
2021-11-29 10:40:08 +0100

    Started implementation of the calendar view

---
+++ ApplicationTier/Models/Event.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ApplicationTier.Models {
+    public class Event {
+        
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+        
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+        
+        [JsonPropertyName("startTime")]
+        public DateTime StartTime { get; set; }
+        
+        [JsonPropertyName("endTime")]
+        public DateTime EndTime { get; set; }
+
+        public override string ToString() {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
