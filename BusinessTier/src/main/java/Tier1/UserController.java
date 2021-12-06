46413011484bbaa4514fc96924f213b41c2847a1
NoHop3 <u_sux@abv.bg>
2021-12-06 11:47:00 +0100

    *Bussiness Tier*  Small adjustements to User class & UserController class for checks + creation of UserTest

--- BusinessTier/src/main/java/Tier1/UserController.java
+++ BusinessTier/src/main/java/Tier1/UserController.java
@@ -50,9 +50,9 @@ public class UserController extends Controller {
   public String addUser(User user) throws Exception
   {
     String isExisting = chechIfExist(user.getEmail(), user.getUsername());
-    String valiedEmail = validateEmail(user.getEmail());
+    user.runChecks();
     cashedUser = super.communicator.addUser(user);
-    return valiedEmail;
+    return user.getEmail();
   }
   public void deleteUser(int userId) throws Exception
   {
@@ -72,35 +72,7 @@ public class UserController extends Controller {
    * checks for User - if email is valid and if it already exists.
    * @return Successful if the email is valid and if the user does not exist.
    */
-  
-  private String validateEmail(String email) {
-    if (!email.contains("@") || !email.substring(email.indexOf("@") + 1).contains("."))
-      throw new IllegalArgumentException("Invalid email format. Email must respect user@host.domain format.");
-    String[] emailParts = email.split("[@._]");
-    if (emailParts.length < 1 || emailParts[0] == null || emailParts[0].isEmpty())
-      throw new IllegalArgumentException("User part of email can't be empty, user@host.domain .");
-    if (emailParts.length < 2 || emailParts[1] == null || emailParts[1].isEmpty())
-      throw new IllegalArgumentException("Host part of email can't be empty, user@host.domain .");
-    if (emailParts.length < 3 || emailParts[2] == null || emailParts[2].isEmpty())
-      throw new IllegalArgumentException("Domain part of email can't be empty, user@host.domain .");
-    if (!emailParts[1].matches("[a-zA-Z0-9]*"))
-      throw new IllegalArgumentException("Host part of email can not contain any symbols, user@host.domain .");
-    if (!emailParts[2].matches("[a-zA-Z0-9]*"))
-      throw new IllegalArgumentException("Domain part of email can not contain any symbols, user@host.domain .");
-    if (emailParts[0].length() > 64)
-      throw new IllegalArgumentException("User part of email can't be more then 64 chars, user@host.domain .");
-    if (emailParts[1].length() > 63)
-      throw new IllegalArgumentException("Host part of email can't be more then 63 chars, user@host.domain .");
-    if (emailParts[2].length() > 63)
-      throw new IllegalArgumentException("Domain part of email can't be more then 63 chars, user@host.domain .");
-    char c = emailParts[1].toUpperCase().charAt(0);
-    if (!('A' <= c && c <= 'Z'))
-      throw new IllegalArgumentException(
-          "The first char of the email host part has to be a letter, user@host.domain .");
-    if (!emailParts[2].matches(".*[a-zA-Z]+.*"))
-      throw new IllegalArgumentException("Domain part of email has to have at least one letter, user@host.domain .");
-    return "Successful";
-  }
+
 
   private String chechIfExist(String email, String username) throws Exception {
     ArrayList<User> users = communicator.fetchUsersFromDatabase();
---
+++ BusinessTier/src/test/java/Shared/UserTest.java
@@ -0,0 +1,121 @@
+package Shared;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class UserTest
+{
+
+  User user;
+
+  @BeforeEach
+  void setUp()
+  {
+    user =new User();
+  }
+
+  @AfterEach
+  void tearDown()
+  {
+
+  }
+
+  @Test
+  void setNull()
+  {
+    /** ID */
+    assertThrows(IllegalArgumentException.class, () -> user.setId(-1));
+
+    /** Email */
+    assertThrows(IllegalArgumentException.class, () -> user.setEmail(""));
+    assertThrows(IllegalArgumentException.class, () -> user.setEmail(" "));
+   // assertThrows(IllegalArgumentException.class, () -> user.setEmail(null));
+
+    /** Username */
+    assertThrows(IllegalArgumentException.class, () -> user.setUsername(""));
+    assertThrows(IllegalArgumentException.class, () -> user.setUsername(" "));
+    assertThrows(IllegalArgumentException.class, () -> user.setUsername(null));
+
+
+    /** Password */
+    assertThrows(IllegalArgumentException.class, () -> user.setPassword(""));
+    assertThrows(IllegalArgumentException.class, () -> user.setPassword(" "));
+    assertThrows(IllegalArgumentException.class, () -> user.setPassword(null));
+  }
+
+  @Test
+  void setOne()
+  {
+    /**ID*/
+    // No cases? //
+
+    /**Email*/
+    assertThrows(IllegalArgumentException.class, () -> user.setEmail("b"));
+    assertThrows(IllegalArgumentException.class, () -> user.setEmail("bob"));
+
+    /**Password*/
+    assertThrows(IllegalArgumentException.class, () -> user.setPassword("M"));
+    assertThrows(IllegalArgumentException.class, () -> user.setPassword("Mi"));
+
+    /**Username*/
+    assertThrows(IllegalArgumentException.class, () -> user.setUsername("b"));
+    assertThrows(IllegalArgumentException.class, () -> user.setUsername("bo"));
+  }
+  @Test
+  void setMany()
+  {
+    /**ID*/
+    // No cases? //
+
+
+    /**Email*/
+    assertThrows(IllegalArgumentException.class, () -> user.setEmail("bobgmail.com"));
+    assertThrows(IllegalArgumentException.class, () -> user.setEmail("bob@gmail"));
+    user.setEmail("bob@gmail.com");
+    assertEquals("bob@gmail.com", user.getEmail());
+    user.setEmail("bob@abv.com");
+    assertEquals("bob@abv.com", user.getEmail());
+    user.setEmail("bob@abv.bg");
+    assertEquals("bob@abv.bg", user.getEmail());
+    user.setEmail("bob@via.dk");
+    assertEquals("bob@via.dk", user.getEmail());
+
+    /**Password*/
+    assertThrows(IllegalArgumentException.class, () -> user.setPassword("Mdgkjgje"));
+    assertThrows(IllegalArgumentException.class, () -> user.setPassword("Asd123"));
+    user.setPassword("Asd123456");
+    assertEquals("Asd123456", user.getPassword());
+
+    /** USername */
+    assertThrows(IllegalArgumentException.class, () -> user.setUsername("thisUsernameHasMoreThan16CharactersAndC"));
+    ;
+  }
+  @Test
+  void setBoundary()
+  {
+    /** No cases ? */
+  }
+
+  @Test
+  void setExceptions() {
+    /**Email*/
+    assertThrows(IllegalArgumentException.class, () -> user.setEmail("bobgmail.com"));
+    assertThrows(IllegalArgumentException.class, () -> user.setEmail("bob@gmail"));
+    assertThrows(IllegalArgumentException.class, () -> user.setEmail("bob.gmail@com"));
+    assertThrows(IllegalArgumentException.class, () -> user.setEmail("bob@gmail."));
+    assertThrows(IllegalArgumentException.class, () -> user.setEmail("@@.."));
+
+    /**Password*/
+    assertThrows(IllegalArgumentException.class, () -> user.setPassword("Mdgkjgje"));
+    assertThrows(IllegalArgumentException.class, () -> user.setPassword("Asd123"));
+    assertThrows(IllegalArgumentException.class, () -> user.setPassword("12343543515435436"));
+    assertThrows(IllegalArgumentException.class, () -> user.setPassword("sdfghjkdurydtzsrertyjy213"));
+
+    /** Username */
+    assertThrows(IllegalArgumentException.class, () -> user.setUsername("thishasexactly15"));
+  }
+}
