364fdc90abde1f603f5604c5a5731374ae2d2157
NoHop3 <u_sux@abv.bg>
2021-12-10 21:45:35 +0100

    DB SIDE : hotfix for EditEvent Batch now successfully edits all Events + their associated address

--- DataAccessTier/Data/EventRepo.cs
+++ DataAccessTier/Data/EventRepo.cs
@@ -33,7 +33,16 @@ namespace DataAccessTier.Data
                 if (address != null)
                 {
                     evt.Address = address;
-                    //evt.AddressId = address.Id;
+                    evt.AddressId = address.Id;
+                }
+                else
+                {
+                    evt.Address.Id = await db.Address.MaxAsync(a => a.Id + 1);
+                
+                    db.Address.Add(evt.Address);
+                    await db.SaveChangesAsync();
+                    address = await db.Address.FirstOrDefaultAsync(a => a.Id == evt.Address.Id);
+                    evt.Address = address;
                 }
                 
                 evt.Id = await db.Event.MaxAsync(e => e.Id) + 1;
@@ -56,8 +65,11 @@ namespace DataAccessTier.Data
             Address address = await db.Address.FirstOrDefaultAsync(ad => ad.StreetName == evt.Address.StreetName && ad.Number == evt.Address.Number && ad.City == evt.Address.City && ad.Country == evt.Address.Country);
             if (address == null)
             {
+                evt.Address.Id = await db.Address.MaxAsync(a => a.Id + 1);
+                
                 db.Address.Add(evt.Address);
-                address = db.Address.OrderBy(ad => ad.Id).Last();
+                await db.SaveChangesAsync();
+                address = await db.Address.FirstOrDefaultAsync(a => a.Id == evt.Address.Id);
 
             }
 
@@ -65,21 +77,31 @@ namespace DataAccessTier.Data
             evt.AddressId = address.Id;
             try
             {
+                
                 await db.BatchUpdate<Event>()
                     .Set(b => b.Title, b => evt.Title)
                     .Set(b => b.Description, b => evt.Description)
                     .Set(b => b.StartTime, b => evt.StartTime)
                     .Set(b => b.EndTime, b => evt.EndTime)
+                    .Set(b => b.AddressId, b => evt.AddressId)
                     .Where(b => b.StartTime == evt.StartTime && b.EndTime == evt.EndTime)
                     .ExecuteAsync();
                 //db.Event.Update(evt);
+                /*
+                     await db.BatchUpdate<Address>()
+                    .Set(a => a.Country, a => address.Country)
+                    .Set(a => a.City, a => address.City)
+                    .Set(a => a.StreetName, a => address.StreetName)
+                    .Set(a => a.Number, a => address.Number)
+                    .Where(a => a.Id == address.Id)
+                    .ExecuteAsync();
+                 */
                 await db.SaveChangesAsync();
 
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
-                Console.WriteLine(e.StackTrace);
             }
             return evt;
         }
